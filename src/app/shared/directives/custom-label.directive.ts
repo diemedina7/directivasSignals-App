714e0b54a4635f88051cc369df755a4cfad7ae5e
Diego Medina <diegocab456@gmail.com>
2023-09-30 16:57:15 -0300

    manejo de errores por directiva

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,9 +1,14 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
 })
 export class CustomLabelDirective implements OnInit {
+
+  private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+  private _errors?: ValidationErrors | null | undefined;
 
   @Input()
   set color( value: string ) {
@@ -11,8 +16,11 @@ export class CustomLabelDirective implements OnInit {
     this.setStyle();
   }
 
-  private htmlElement?: ElementRef<HTMLElement>;
-  private _color: string = 'red';
+  @Input()
+  set errors( value: ValidationErrors | null | undefined ) {
+    this._errors = value;
+    this.setErrorMessage();
+  }
 
   constructor( private el: ElementRef<HTMLElement> ) {
     console.log(el);
@@ -28,6 +36,36 @@ export class CustomLabelDirective implements OnInit {
       return;
 
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  public setErrorMessage(): void {
+    if (!this.htmlElement)
+      return;
+
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerHTML = 'No hay errores';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerHTML = 'Este campo es requerido';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      const min = this._errors['minlength']['requiredLength'];
+      const current = this._errors['minlength']['actualLength'];
+
+      this.htmlElement.nativeElement.innerHTML = `Este campo no tiene los caracteres minimos (${ min }) - actual: ${ current }`;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerHTML = 'Este campo no tiene formato válido de email';
+      return;
+    }
   }
 
 }
