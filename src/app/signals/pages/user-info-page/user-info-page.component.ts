b8101082ca7b5de61a242a67cf66556f8ad95953
Diego Medina <diegocab456@gmail.com>
2023-10-02 23:56:21 -0300

    user info page con signals

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UserService } from '../../services/user-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrls: ['./user-info-page.component.css']
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
 
+  private usersService = inject( UserService );
+  public userId = signal(1);
+
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() );
+  }
+
+  public loadUser( id: number ) {
+    if (id <= 0)
+      return;
+
+    this.userId.set( id );
+    this.currentUser.set( undefined );
+
+    this.usersService.getUserById( id )
+      .subscribe( user => this.currentUser.set( user ) )
+  }
 }
