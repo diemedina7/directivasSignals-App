1ef85fc5a71ef1b0cd6c08b1358b2bcec903ff87
Diego Medina <diegocab456@gmail.com>
2023-10-02 22:45:22 -0300

    signal writeable y signal readonly - counterPage

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
 
+  public counter = signal(10);                                               //signal writeable
+  public squareCounter = computed( () => this.counter() * this.counter() );  //signal onlyread
+
+  public increaseBy( value: number ) {
+    this.counter.update( current => current + value );
+  }
 }
