78b979ac69137dd8a60a23495b7d20682fcdb5d9
Diego Medina <diegocab456@gmail.com>
2023-10-03 23:08:17 -0300

    mutate signals - propertiesPage

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -6,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 1,
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    last_name: "Bluth",
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  });
+
+  public fullName = computed<string>( () => `${ this.user()!.first_name } ${ this.user()!.last_name }`);
+
+  public onFieldUpdated( field: keyof User, value: string ): void {
+    this.user.mutate( current => {
+      switch( field ) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+    });
+  }
 }
